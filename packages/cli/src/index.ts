9ed362e8a970ce500bde63065c602af86850ec26
#V0ID <hey@v0id.me>
2025-10-06 00:41:55 +0300

    chore(cli): bump version to 0.2.6 in package.json and update CLI version in index.ts

--- packages/cli/src/index.ts
+++ packages/cli/src/index.ts
@@ -15,7 +15,7 @@ dotenv.config({
 program
   .name("nyron")
   .description("Nyron CLI — smarter multi-package versioning")
-  .version("0.1.0")
+  .version("0.2.6")
 
 // -----------------------------
 // bump
