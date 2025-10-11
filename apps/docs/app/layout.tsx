5cd927559bf3511222fafa47381e876aef91a158
#V0ID <hey@v0id.me>
2025-10-11 20:28:24 +0300

    chore(dependencies): update bun.lock and package.json to rename apps/docs and apps/nyron-bot, add next-themes dependency, and adjust layout component to include ThemeProvider

--- apps/docs/app/layout.tsx
+++ apps/docs/app/layout.tsx
@@ -1,7 +1,7 @@
 import '@/app/global.css';
 import { RootProvider } from 'fumadocs-ui/provider/next';
 import { Inter } from 'next/font/google';
-
+import { ThemeProvider } from 'next-themes';
 const inter = Inter({
   subsets: ['latin'],
 });
@@ -10,7 +10,12 @@ export default function Layout({ children }: LayoutProps<'/'>) {
   return (
     <html lang="en" className={inter.className} suppressHydrationWarning>
       <body className="flex flex-col min-h-screen">
-        <RootProvider>{children}</RootProvider>
+        <ThemeProvider attribute="class" defaultTheme="light">
+          <RootProvider theme={{
+            enabled: false,
+          }}>
+            {children}</RootProvider>
+        </ThemeProvider>
       </body>
     </html>
   );
